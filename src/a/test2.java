36ef894648917359416628aed4ab764c865d7daa
kedar <kedar@kedar-PC>
2020-07-24 07:00:00 -0700

    commiting to branch

---
+++ src/a/test2.java
@@ -0,0 +1,13 @@
+package a;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  for(int i =1; i<=2; i++)
+  {
+	 System.out.println("hello"); 
+  }
+	
+	}
+}
